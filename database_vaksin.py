5862187725870f126648fcd31617fb525ed6657b
Jacindaardina12 <jacindaardinagestyaki@gmail.com>
2025-12-25 19:44:55 +0700

    fix2

--- database_vaksin.py
+++ database_vaksin.py
@@ -1,14 +1,15 @@
 from sqlalchemy import create_engine, text
 import pandas as pd
 
+# --- Konfigurasi koneksi ---
 DB_USER = "root"
-DB_PASS = "72741516"
-DB_HOST = "localhost"
-DB_PORT = "3306"
-DB_NAME = "db_vaksin"
+DB_PASS = "cCoiaAEpiqVgAaqcAyjdxORwIDyKWGZw"
+DB_HOST = "tramway.proxy.rlwy.net"
+DB_PORT = "27684"
+DB_NAME = "railway"
 
 engine = create_engine(
-    f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}",
+    f"mysql://root:cCoiaAEpiqVgAaqcAyjdxORwIDyKWGZw@tramway.proxy.rlwy.net:27684/railway",
     pool_pre_ping=True
 )
 
